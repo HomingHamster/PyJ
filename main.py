d55cb35ef07c7c31380c85561ae3be7e4bc85112
Andrew Cooper <acoops01@gmail.com>
2012-03-17 22:11:16 +0000

    added rest of buttons and a menu bar with and exit command

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ class GUI(Tk):
     def __init__(self):
         Tk.__init__(self)
         self.createMainWindow()
+        self.config(menu=self.menuBar())
     def createMainWindow(self):
         mainFrame = Frame(self)
         buttonFrame = self.createButtonFrame(mainFrame)
@@ -15,17 +16,26 @@ class GUI(Tk):
         buttonFrame = Frame(parent)
         newClassButton = Button(buttonFrame, text="New Class")
         newClassButton.pack(fill=X)
-	newRelButton = Button(buttonFrame, text="New Relationship")
-	newRelButton.pack()
-	clearCanvasButton = Button(buttonFrame, text="Clear Canvas")
-	clearCanvasButton.pack()
-	generateJavaCodeButton = Button(buttonFrame, text="Generate Java Code")
-	generateJavaCodeButton.pack()
-	editLinkButton = Button(buttonFrame, text="Edit/Delete a Relationship")
-	editLinkButton.pack()
-	editClassButton = Button(buttonFrame, text="Edit/Delete a Class")
-	editClassButton.pack()
+        newRelButton = Button(buttonFrame, text="New Relationship")
+        newRelButton.pack(fill=X)
+        clearCanvasButton = Button(buttonFrame, text="Clear Canvas")
+        clearCanvasButton.pack(fill=X)
+        generateJavaCodeButton = Button(buttonFrame, text="Generate Java Code")
+        generateJavaCodeButton.pack(fill=X)
+        editLinkButton = Button(buttonFrame, text="Edit/Delete a Relationship")
+        editLinkButton.pack(fill=X)
+        editClassButton = Button(buttonFrame, text="Edit/Delete a Class")
+        editClassButton.pack(fill=X)
         return buttonFrame
+        
+    def menuBar(self):
+        menubar = Menu(self)
+        
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Exit", command=self.quit)
+        
+        menubar.add_cascade(label="File", menu=filemenu)
+        return menubar
 
 if __name__=='__main__':
     app = GUI()
