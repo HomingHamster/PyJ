42290ea114d5e160dcdcf554ce9c10af670bc3f3
Felix Farquharson <fef@aber.ac.uk>
2012-03-17 21:21:26 +0000

    *added a class for the gui in the main.py file *added a function to create the main window *added a function to create a frame with the buttons on the left hand side, plus added one button for example. *added (I hope) a canvas to be used later for holding the class info.

---
+++ main.py
@@ -0,0 +1,22 @@
+from Tkinter import *
+
+class GUI(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+        self.createMainWindow()
+    def createMainWindow(self):
+        mainFrame = Frame(self)
+        buttonFrame = self.createButtonFrame(mainFrame)
+        buttonFrame.pack(side=LEFT, fill=Y)
+        mainCanvas = Canvas(self)
+        mainCanvas.pack(side=RIGHT, fill=BOTH, expand=1)
+        mainFrame.pack(fill=BOTH, expand=1)
+    def createButtonFrame(self, parent):
+        buttonFrame = Frame(parent)
+        newClassButton = Button(buttonFrame, text="New Class")
+        newClassButton.pack(fill=X)
+        return buttonFrame
+
+if __name__=='__main__':
+    app = GUI()
+    app.mainloop()
